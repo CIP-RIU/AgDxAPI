15fb4c0e42c5f55c77903096307075bf8af3380e
Sammy Barasa <barsamms@gmail.com>
2020-12-24 10:20:25 +0300

    First commit

---
+++ src/main/kotlin/com/cip/agdxapi/AgdxapiApplication.kt
@@ -0,0 +1,11 @@
+package com.cip.agdxapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgdxapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<AgdxapiApplication>(*args)
+}
