7e5a7dd310ba1aed97ae6848b6246450366456cf
Tsobu Developer <dev@tsobu.co.ke>
2021-01-11 14:30:22 +0300

    Folder rearrangment

---
+++ api/src/main/kotlin/com/cip/agdxapi/api/AgDxApiApp.kt
@@ -0,0 +1,11 @@
+package com.cip.agdxapi.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgDxApiApp
+
+fun main(args: Array<String>) {
+    runApplication<AgDxApiApp>(*args)
+}
