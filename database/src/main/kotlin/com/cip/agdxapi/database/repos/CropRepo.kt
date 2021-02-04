7131aa812ec4f123c22aef6679d107fa9b374b40
Sammy Barasa <barsamms@gmail.com>
2021-02-04 13:01:45 +0300

    Added crop and pest repos

--- database/src/main/kotlin/com/cip/agdxapi/database/repos/CropRepo.kt
+++ database/src/main/kotlin/com/cip/agdxapi/database/repos/CropRepo.kt
@@ -1,12 +1,14 @@
 package com.cip.agdxapi.database.repos
 
+import com.cip.agdxapi.database.entities.CropEntity
 import com.cip.agdxapi.database.entities.CropPestEntity
+import com.cip.agdxapi.database.entities.PestEntity
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CropPestRepo : PagingAndSortingRepository<CropPestEntity, Long> {
-    override fun findAll(pageable: Pageable): Page<CropPestEntity>
+interface CropRepo : PagingAndSortingRepository<CropEntity, Long> {
+    override fun findAll(pageable: Pageable): Page<CropEntity>
 }
--- database/src/main/kotlin/com/cip/agdxapi/database/repos/PestRepo.kt
+++ database/src/main/kotlin/com/cip/agdxapi/database/repos/PestRepo.kt
@@ -1,12 +1,13 @@
 package com.cip.agdxapi.database.repos
 
 import com.cip.agdxapi.database.entities.CropPestEntity
+import com.cip.agdxapi.database.entities.PestEntity
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CropPestRepo : PagingAndSortingRepository<CropPestEntity, Long> {
-    override fun findAll(pageable: Pageable): Page<CropPestEntity>
+interface PestRepo : PagingAndSortingRepository<PestEntity, Long> {
+    override fun findAll(pageable: Pageable): Page<PestEntity>
 }
