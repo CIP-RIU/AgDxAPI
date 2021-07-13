e77825ceb33bd2886a49103a51d49cd0376d6ad0
Sammy Barasa <barsamms@gmail.com>
2021-07-13 12:16:09 +0300

    feat: security
    added authenitcation mechanism to the OpenAPI spec

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+<a name="unreleased"></a>
+## [Unreleased]
+
+
+<a name="v0.0.1"></a>
+## v0.0.1 - 2021-04-05
+
+[Unreleased]: https://github.com/CIP-RIU/AgDxAPI/compare/v0.0.1...HEAD
--- api/src/main/kotlin/com/cip/agdxapi/api/config/OpenApiConfig.kt
+++ api/src/main/kotlin/com/cip/agdxapi/api/config/OpenApiConfig.kt
@@ -1,19 +1,18 @@
 package com.cip.agdxapi.api.config
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
-import io.swagger.v3.oas.annotations.security.SecurityScheme
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.ExternalDocumentation
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.info.License
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@SecurityScheme(name = "api", scheme = "basic", type = SecuritySchemeType.HTTP, `in` = SecuritySchemeIn.HEADER)
-class OpenApiConfig() {
+//@SecurityScheme(name = "api", scheme = "basic", type = SecuritySchemeType.HTTP, `in` = SecuritySchemeIn.HEADER)
+class OpenApiConfig {
     @Bean
     fun api(
         @Value("\${cip.version}") appVersion: String,
@@ -25,6 +24,17 @@ class OpenApiConfig() {
                     .description("$appName facilitates sharing of crop pest and disease data between platforms and/or by providing links between complementary ICT tools where appropriate.")
                     .version(appVersion)
                     .license(License().name("Apache 2.0").url("http://springdoc.org"))
+            )
+            .components(
+                Components().addSecuritySchemes(
+                    "jwt",
+                    SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                        .`in`(SecurityScheme.In.HEADER).name("Authorization")
+                ).addSecuritySchemes(
+                    "api",
+                    SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")
+                        .`in`(SecurityScheme.In.HEADER).name("Authorization")
+                )
             )
             .externalDocs(
                 ExternalDocumentation()
